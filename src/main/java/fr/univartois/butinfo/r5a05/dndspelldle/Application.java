1b4d9949528ddc39ce640655eff5949d9d801a21
ceestos <jules.langagne@gmail.com>
2024-10-16 11:27:01 +0200

    Init Project + Adding Gradle Setting and Dependencies

---
+++ src/main/java/fr/univartois/butinfo/r5a05/dndspelldle/Application.java
@@ -0,0 +1,13 @@
+package fr.univartois.butinfo.r5a05.dndspelldle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univartois/butinfo/r5a05/dndspelldle/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univartois.butinfo.r5a05.dndspelldle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
